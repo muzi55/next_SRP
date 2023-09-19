d66f776b48860a2b6c86bc7d3af3c2af52321b02
muzi55 <muzi5@kakao.com>
2023-09-20 04:45:14 +0900

    리드미 수정

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d).{6,}$/;
+
+const Form = (): JSX.Element => {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const validateLoginFormhandle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const validateEmailReg = EMAIL_REGEX.test(email);
+    const validatePasswordReg = PASSWORD_REGEX.test(password);
+
+    if (!validateEmailReg) return alert("이메일을 확인해라");
+    if (!validatePasswordReg) return alert("비밀번호를 확인해라");
+
+    router.push("home");
+  };
+
+  return (
+    <>
+      <form action="POST" onSubmit={(e) => validateLoginFormhandle(e)}>
+        <div>
+          <label htmlFor="email">이메일 :</label>
+          <input onChange={(e) => setEmail(e.target.value)} className="ml-2 px-2 py-1 border border-black rounded-lg" type="text" id="email" value={email} />
+          <label htmlFor="password">비밀번호 :</label>
+          <input onChange={(e) => setPassword(e.target.value)} className="ml-2 px-2 py-1 border border-black rounded-lg" type="password" id="password" value={password} />
+        </div>
+        <button>로그인</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <h2>로그인 완료!</h2>
+      <p>짝짝짝</p>
+    </>
+  );
+};
+
+export default Home;
